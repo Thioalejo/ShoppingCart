2c9f06fdb2a75430de02850cf0ffa2e2ce9099bb
Alejandro <alejo.saraza@gmail.com>
2017-06-17 10:25:48 -0500

    Se agrego carpeta y clase servicio encargado del CRUD en memoria.

---
+++ ShoppingCart/ShoppingCart/Service/ProductService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Collections.ObjectModel;
+using ShoppingCart.Model;
+
+namespace ShoppingCart.Service
+{
+    public class ProductService
+    {
+        public ObservableCollection<ProductModel> products { get; set; }
+
+        public ProductService()
+        {
+            if(products == null)
+            {
+                products = new ObservableCollection<ProductModel>();
+            }
+        }
+
+        public ObservableCollection<ProductModel> Consultar()
+        {
+            return products;
+        }
+
+        public void Guardar(ProductModel modelo)
+        {
+            products.Add(modelo);
+        }
+
+        public void Modificar(ProductModel modelo)
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                if (products[i].Id == modelo.Id)
+                {
+                    products[i] = modelo;
+                }
+            }
+        }
+
+
+        public void Eliminar(int idProduct)
+        {
+            ProductModel modelo = products.FirstOrDefault(p => p.Id == idProduct);
+            products.Remove(modelo);
+        }
+    }
+}
